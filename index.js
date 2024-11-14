03ff87628258fc7487a95dc866bb333423f9ecc5
MaryaLauraRS <117856098+MaryaLauraRS@users.noreply.github.com>
2024-11-14 14:21:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const prisma = require('./prisma/prismaClient')
+const cors = require('cors')
 
 const express= require('express')
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 const authRoutes = require('./routes/authRoutes')
 
